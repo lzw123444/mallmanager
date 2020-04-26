e28f4e30775cea27dbab36a2b8592a926a3d2938
Zhiwei Liu <1484985146@qq.com>
2020-04-26 17:04:26 +0800

    显示home组件

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -31,6 +31,7 @@ export default {
           meta: {msg, status}
         } = res.data
         if (status === 200) {
+          this.$router.push({name: 'home'})
           this.$message.success(msg)
         } else {
           this.$message.error(msg)
