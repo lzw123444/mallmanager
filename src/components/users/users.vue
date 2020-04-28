ee9c8c64055d51446bda3b97fae4033d69a76b6c
Zhiwei Liu <1484985146@qq.com>
2020-04-28 10:03:55 +0800

    日期格式处理-状态开关-操作按钮

--- src/components/users/users.vue
+++ src/components/users/users.vue
@@ -17,20 +17,36 @@
         </el-col>
       </el-row>
       <!-- 3.表格 -->
-       <el-table :data="tableData" style="width: 100%">
+       <el-table :data="userlist" style="width: 100%">
          <el-table-column label="#"  type="index">
          </el-table-column>
-         <el-table-column prop="name" label="姓名" width="180">
+         <el-table-column prop="username" label="姓名" width="180">
          </el-table-column>
-         <el-table-column prop="e-mail" label="邮箱" width="180">
+         <el-table-column prop="email" label="邮箱" width="180">
          </el-table-column>
-         <el-table-column prop="name" label="电话">
+         <el-table-column prop="mobile" label="电话">
          </el-table-column>
-         <el-table-column prop="name" label="创建日期">
+         <el-table-column  label="创建日期">
+           <!-- 如果单元格内容现实的不是字符串（文本），需要给被显示的内容外层包裹一个template标签 -->
+           <template slot-scope="scope">
+             <!-- template内部要用数据，需设置一个slot-scope属性,该属性的值为要用数据的数据源userlist，该数据源为el-table绑定的数据userlist -->
+             <!-- scope.row是数组中的每个对象 -->
+             <!-- slot-scope的值会自动去找上一级最外层标签的数据源作为自己的值 -->
+            {{scope.row.create_time|fmtdate}}
+           </template>
          </el-table-column>
-          <el-table-column prop="name" label="用户状态">
+         <el-table-column  label="用户状态">
+             <template slot-scope="scope">
+               <el-switch v-model="scope.row.mg_state" active-color="#13ce66" inactive-color="#ff4949">
+               </el-switch>
+             </template>
          </el-table-column>
           <el-table-column prop="name" label="操作">
+            <template>
+              <el-button size="mini" plain type="primary" icon="el-icon-edit" circle></el-button>
+              <el-button size="mini" plain type="success" icon="el-icon-check" circle></el-button>
+              <el-button size="mini" plain type="danger" icon="el-icon-delete" circle></el-button>
+            </template>
          </el-table-column>
        </el-table>
     </el-card>
@@ -43,23 +59,8 @@ export default {
       query: '',
       pagenum: 1,
       pagesize: 2,
-      tableData: [{
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-04',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1517 弄'
-      }, {
-        date: '2016-05-01',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1519 弄'
-      }, {
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1516 弄'
-      }]
+      total: 0,
+      userlist: []
     }
   },
   // 钩子函数，用于调用getUserList方法
@@ -74,6 +75,18 @@ export default {
       this.$http.defaults.headers.common['Authorization'] = AUTH_TOKEN
       const res = await this.$http.get(`users?query=${this.query}&pagenum=${this.pagenum}&pagesize=${this.pagesize}`)
       console.log(res)
+      // 解构赋值，获取数据
+      const {data: {users, total}, meta: {msg, status}} = res.data
+      if (status === 200) {
+        // 给数据列表赋值
+        this.userlist = users
+        // 给total赋值
+        this.total = total
+        // 提示信息
+        this.$message.success(msg)
+      } else {
+        this.$message.warning(msg)
+      }
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -7,11 +7,17 @@ import 'element-ui/lib/theme-chalk/index.css'
 import router from './router'
 import '@/assets/css/reset.css'
 import myHttpServe from '@/plugins/http.js'
+import moment from 'moment'
+
 // 使用vue插件
 Vue.use(ElementUI)
 Vue.use(myHttpServe)
 Vue.config.productionTip = false
 /* eslint-disable no-new */
+// 设置全局过滤器
+Vue.filter('fmtdate', (v) => {
+  return moment(v).format('YYYY-MM-DD')
+})
 new Vue({
   el: '#app',
   router,
